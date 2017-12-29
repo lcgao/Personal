8ff21c5db24177957b7d8b47ddd2ce5cccebe8fd
lcgao <963818895@qq.com>
2017-12-29 11:37:49 +0800

    change package name..

---
+++ app/src/main/java/com/lcgao/personal/adapter/CommonAdapter.java
@@ -0,0 +1,52 @@
+package com.lcgao.personal.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by lcgao on 2017/12/28.
+ */
+
+public abstract class CommonAdapter<T> extends RecyclerView.Adapter<ViewHolder>{
+    private List<T> mData;
+    private Context mContext;
+    private LayoutInflater mInflate;
+    private int mLayoutId;
+
+    public CommonAdapter(Context context, int layoutId, List<T> data){
+        this.mContext = context;
+        this.mLayoutId = layoutId;
+        this.mData = data;
+        this.mInflate = LayoutInflater.from(context);
+    }
+
+    public void setData(List<T> data){
+        this.mData = data;
+    }
+
+    public void replaceData(List<T> data){
+        setData(data);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return ViewHolder.get(mContext, parent, mLayoutId);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        convert(holder, mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public abstract void convert(ViewHolder holder, T t);
+}
---
+++ app/src/main/java/com/lcgao/personal/util/LogUtil.java
@@ -0,0 +1,28 @@
+package com.lcgao.personal.util;
+
+import android.util.Log;
+
+/**
+ * Created by lcgao on 2017/6/7.
+ */
+
+public class LogUtil {
+    public static final String TAG = "log_personal";
+
+    public static void d(String content) {
+        Log.d(TAG, content);
+    }
+    public static void d(Object o){
+        d(o + "");
+    }
+
+    public static void l(String content) {
+        String methodName = new Exception().getStackTrace()[1].getMethodName();
+        String classLocation = new Exception().getStackTrace()[1].getClassName();
+        Log.d(TAG, classLocation + "." + methodName + ": " + content);
+    }
+
+    public static void l(){
+        l("");
+    }
+}
