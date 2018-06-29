3050c514ed488c84129d9984e500e23398740c2b
lcgao <963818895@qq.com>
2018-06-29 11:44:02 +0800

    组件化musicmodule

--- app/src/main/java/com/lcgao/personal/MainActivity.java
+++ app/src/main/java/com/lcgao/personal/MainActivity.java
@@ -13,8 +13,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
---
+++ app/src/main/java/com/lcgao/personal/util/RouteUtil.java
@@ -0,0 +1,5 @@
+package com.lcgao.personal.util;
+
+public class RouteUtil {
+    public static final String MODULE_MUSIC_MAIN_ACTIVITY_URL = "/music/main_activity";
+}
